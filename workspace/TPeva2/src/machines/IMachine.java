1988ae749416dcebe0355b3720f62b2281052c4f
zerbino <r2mart10@gmail.com>
2013-05-21 10:44:29 +0200

    intermediaire

--- workspace/TPeva2/src/machines/IMachine.java
+++ workspace/TPeva2/src/machines/IMachine.java
@@ -10,11 +10,19 @@ public interface IMachine extends Remote {
 	
 	public void calculDroit();
 	
+	public void calcul();
+	
 	public void recevoirNotifPanne(String idVoisin);
 	
 	public void recevoirNotifRepar(String idVoisin);
 	
 	public void reparation();
+	
+	public String getId();
+	
+	public Machine getVoisinGauche();
+	
+	public Machine getVoisinDroit();
 	
 	
 
---
+++ workspace/TPeva2/src/machines/Machine.java
@@ -0,0 +1,130 @@
+package machines;
+
+import G.IG;
+
+public class Machine implements IMachine {
+
+	private Machine voisinGauche, voisinDroit;
+	private IG controleur;
+	private String id;
+	private boolean voisinGaucheBreakdown = false, voisinDroitBreakdown = false;
+	
+	private boolean breakdown = false;
+
+	public Machine(String id, Machine voisinGauche, Machine voisinDroit) {
+		super();
+		this.voisinGauche = voisinGauche;
+		this.voisinDroit = voisinDroit;
+		this.id = id;
+	}
+	
+	
+
+	public boolean isVoisinGaucheBreakdown() {
+		return voisinGaucheBreakdown;
+	}
+
+
+
+
+
+
+	public boolean isVoisinDroitBreakdown() {
+		return voisinDroitBreakdown;
+	}
+
+
+
+
+	
+
+
+
+	@Override
+	public void panne() {
+		double rand = Math.random();
+		if(rand>0.9){
+			this.breakdown = true;
+			controleur.notifPanne(this.getId());
+			
+		}
+		
+
+	}
+
+	@Override
+	public void calculGauche() {
+		System.out.println("calculs sur la machine "+this.getId()+" delegue a la machine "+this.getVoisinGauche());
+		this.getVoisinGauche().calcul();
+
+	}
+
+	@Override
+	public void calculDroit() {
+		System.out.println("calculs sur la machine "+this.getId()+" delegue a la machine "+this.getVoisinDroit());
+
+
+	}
+	
+	public void setBreakdownVoisin(boolean breakdown, String idVoisin){
+		if(idVoisin.equals(this.getVoisinGauche().getId())){
+			this.voisinGaucheBreakdown = breakdown;
+		}
+		else{
+			if(idVoisin.equals(this.getVoisinDroit().getId())){
+				this.voisinDroitBreakdown = breakdown;
+			}
+			else{
+				
+			}
+		}
+	}
+
+	@Override
+	public void recevoirNotifPanne(String idVoisin) {
+		
+		this.setBreakdownVoisin(true, idVoisin);
+
+	}
+
+	@Override
+	public void recevoirNotifRepar(String idVoisin) {
+		this.setBreakdownVoisin(false, idVoisin);
+
+	}
+
+	@Override
+	public void reparation() {
+		this.breakdown = false;
+
+	}
+
+	@Override
+	public String getId() {
+		return this.id;
+	}
+
+	@Override
+	public Machine getVoisinGauche() {
+		return voisinGauche;
+	}
+
+	@Override
+	public Machine getVoisinDroit() {
+		return this.voisinDroit;
+	}
+
+	@Override
+	public void calcul() {
+		if(!breakdown){
+			if(!this.isVoisinDroitBreakdown()){
+				this.voisinDroit.calcul();
+			}
+			if(!this.isVoisinGaucheBreakdown()){
+				this.voisinGauche.calcul();
+			}
+		}
+		
+	}
+
+}
