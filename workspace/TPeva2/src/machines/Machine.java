28b90fea69250488617266c0381a82ac17c822b2
zerbino <r2mart10@gmail.com>
2013-05-21 11:13:11 +0200

    machin

--- workspace/TPeva2/src/machines/Machine.java
+++ workspace/TPeva2/src/machines/Machine.java
@@ -1,5 +1,9 @@
 package machines;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 import G.IG;
 
 public class Machine implements IMachine {
@@ -7,80 +11,66 @@ public class Machine implements IMachine {
 	private Machine voisinGauche, voisinDroit;
 	private IG controleur;
 	private String id;
-	private boolean voisinGaucheBreakdown = false, voisinDroitBreakdown = false;
-	
+	private boolean voisinGaucheBreakdown = false,
+			voisinDroitBreakdown = false;
+
 	private boolean breakdown = false;
 
 	public Machine(String id) {
 		super();
 		this.id = id;
 	}
-	
-	
 
 	public boolean isVoisinGaucheBreakdown() {
 		return voisinGaucheBreakdown;
 	}
 
-
-
-
-
-
 	public boolean isVoisinDroitBreakdown() {
 		return voisinDroitBreakdown;
 	}
 
-
-
-
-	
-
-
-
 	@Override
 	public void panne() {
 		double rand = Math.random();
-		if(rand>0.9){
+		if (rand > 0.9) {
 			this.breakdown = true;
 			controleur.notifPanne(this.getId());
-			
+
 		}
-		
 
 	}
 
 	@Override
 	public void calculGauche() {
-		System.out.println("calculs sur la machine "+this.getId()+" delegue a la machine "+this.getVoisinGauche());
+		System.out.println("calculs sur la machine " + this.getId()
+				+ " delegue a la machine " + this.getVoisinGauche());
 		this.getVoisinGauche().calcul();
 
 	}
 
 	@Override
 	public void calculDroit() {
-		System.out.println("calculs sur la machine "+this.getId()+" delegue a la machine "+this.getVoisinDroit());
-
+		System.out.println("calculs sur la machine " + this.getId()
+				+ " delegue a la machine " + this.getVoisinDroit());
+		this.getVoisinDroit().calcul();
 
 	}
-	
-	public void setBreakdownVoisin(boolean breakdown, String idVoisin){
-		if(idVoisin.equals(this.getVoisinGauche().getId())){
+
+	public void setBreakdownVoisin(boolean breakdown, String idVoisin) {
+		if (idVoisin.equals(this.getVoisinGauche().getId())) {
 			this.voisinGaucheBreakdown = breakdown;
-		}
-		else{
-			if(idVoisin.equals(this.getVoisinDroit().getId())){
+		} else {
+			if (idVoisin.equals(this.getVoisinDroit().getId())) {
 				this.voisinDroitBreakdown = breakdown;
-			}
-			else{
-				
+			} else {
+
 			}
 		}
 	}
 
 	@Override
 	public void recevoirNotifPanne(String idVoisin) {
-		
+
 		this.setBreakdownVoisin(true, idVoisin);
 
 	}
@@ -111,37 +101,67 @@ public class Machine implements IMachine {
 	public Machine getVoisinDroit() {
 		return this.voisinDroit;
 	}
-	
-	
 
 	public void setVoisinGauche(Machine voisinGauche) {
 		this.voisinGauche = voisinGauche;
 	}
 
-
-
 	public void setVoisinDroit(Machine voisinDroit) {
 		this.voisinDroit = voisinDroit;
 	}
 
-
-
 	@Override
 	public void calcul() {
 		this.panne();
-		if(!breakdown){
-			if(!this.isVoisinDroitBreakdown()){
-				this.voisinDroit.calcul();
+		if (!breakdown) {
+			System.out.println("Calcul de la machine "+this.getId());
+			if (!this.isVoisinDroitBreakdown()) {
+				this.calculDroit();
 			}
-			if(!this.isVoisinGaucheBreakdown()){
-				this.voisinGauche.calcul();
+			if (!this.isVoisinGaucheBreakdown()) {
+				this.calculGauche();
 			}
 		}
-		
-	}
-	
-	public static void main(String[] args) {
-		
+
 	}
 
+	public static void main(String[] args) {
+		try {
+			String name = "G";
+			Registry registry = LocateRegistry.getRegistry();
+			IG monG = (IG) registry.lookup(name);
+			System.out.println("Serveur trouvé");
+
+			Machine m1 = new Machine("m1");
+			Machine m2 = new Machine("m2");
+			Machine m3 = new Machine("m3");
+			Machine m4 = new Machine("m4");
+			m1.setVoisinGauche(m4);
+			m1.setVoisinDroit(m2);
+			IMachine stub1 = (IMachine) UnicastRemoteObject.exportObject(m1, 2);
+
+			m2.setVoisinGauche(m1);
+			m2.setVoisinDroit(m3);
+			IMachine stub2 = (IMachine) UnicastRemoteObject.exportObject(m2, 3);
+
+			m3.setVoisinGauche(m2);
+			m3.setVoisinDroit(m4);
+			IMachine stub3 = (IMachine) UnicastRemoteObject.exportObject(m3, 4);
+
+			m4.setVoisinGauche(m3);
+			m4.setVoisinDroit(m1);
+			IMachine stub4 = (IMachine) UnicastRemoteObject.exportObject(m4, 5);
+
+			monG.addMachines(stub1);
+			monG.addMachines(stub2);
+			monG.addMachines(stub3);
+			monG.addMachines(stub4);
+			
+			m1.calcul();
+
+		} catch (Exception e) {
+			System.err.println("Client exception:");
+			e.printStackTrace();
+		}
+	}
 }
