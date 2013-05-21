cb6a41a1c6c2ac13e562f349f6814af8e2046188
Rambo21 <kevin.llopart@gmail.com>
2013-05-21 11:02:11 +0200

    truc

--- workspace/TPeva2/src/G/IG.java
+++ workspace/TPeva2/src/G/IG.java
@@ -1,9 +1,13 @@
 package G;
 
 import java.rmi.*;
+
+import machines.IMachine;
 public interface IG extends Remote{
 
 	public void notifPanne(String idVoisin);
 
 	public void notifRepar(String idVoisin);
+	
+	public void addMachines(IMachine c);
 }
